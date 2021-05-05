ded97f02a5af17a50cffbb00cc41a960dcc3ea9f
mov51 <mov51@mov51.net>
2021-05-05 13:37:55 -0400

    Added comments, cleaned up imports and adjusted priority.

--- src/main/java/net/mov51/listeners/PlayerKill.java
+++ src/main/java/net/mov51/listeners/PlayerKill.java
@@ -1,16 +1,20 @@
 package net.mov51.listeners;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class PlayerKill implements Listener {
-    @EventHandler(priority = EventPriority.HIGHEST)
+    //Listen for Player Death event
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onBreak(PlayerDeathEvent e) {
+        //Get the killer and check if it's null
+        //Apparently this only returns players and is null if the killer is not a player.
         if(e.getEntity().getKiller() != null){
+            //Turns on keep inventory for this one event for the killed player
             e.setKeepInventory(true);
+            //Clears the items for the event because they drop anyway (Yay!)
             e.getDrops().clear();
             System.out.println("keep inventory saved");
         }
