8f277155d97a44dc15195d87314e11754d0aefe8
mov51 <mov51@mov51.net>
2021-05-05 20:12:49 -0400

    Fix typo's

--- src/main/java/net/mov51/PlayerHeadHelper.java
+++ src/main/java/net/mov51/PlayerHeadHelper.java
@@ -8,21 +8,21 @@ import org.bukkit.inventory.meta.SkullMeta;
 
 public class PlayerHeadHelper {
     public static void dropPlayerHead(Player p, Location l){
-        //get the provided player's name
+        //Get the provided player's name
         String playerName = p.getName();
-        //create the item
+        //Create the item
         ItemStack playerHead = new ItemStack(Material.PLAYER_HEAD, 1);
-        //generate the metadata from the item
+        //Generate the metadata from the item
         SkullMeta meta = (SkullMeta)playerHead.getItemMeta();
-        //make sure that meta was properly initialized, I guess? Thanks IDE
+        //Make sure that meta was properly initialized, I guess? Thanks IDE
         if (meta != null) {
-            //ad their name to the skull
+            //Add the player's name to the skull
             meta.setDisplayName(playerName + "'s Head");
-            //make it their skull
+            //Make it their skull
             meta.setOwningPlayer(p);
-            //add the generated meta to the skull
+            //Add the generated meta to the skull
             playerHead.setItemMeta(meta);
-            //and finally, summon the item
+            //And finally, summon the item at the provided location
             p.getWorld().dropItemNaturally(l, playerHead);
         }
     }
